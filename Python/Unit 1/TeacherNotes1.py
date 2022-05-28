d19742bb54b747548bc27f6087d894f7998fc1cd
Jayx20 <Jayx20@users.noreply.github.com>
2022-05-27 19:12:46 -0500

    notes/init

---
+++ Python/Unit 1/TeacherNotes1.py
@@ -0,0 +1,48 @@
+# a comment is a line in your program that starts with a # character. Anything after the # will appear but not do anything in the code, until a new line starts.
+
+# Declaring a variable
+joe = 1
+
+# Declaring another variable
+name = "Obama"
+
+# Bad variable (uncomment)
+# 2joe = "bruh"
+
+# printing
+
+print("Hello Joe!")
+
+print("Hello "+name)
+
+# redefining a value
+
+name = "Joe"
+print("Hello "+name)
+
+# editing a string variable
+
+full_name = name + ' Mama!' # you can use single or double quotes
+
+# using a comma in print()
+print("Hello ", full_name) # note that it adds a space
+
+# Numbers
+
+print(joe + 2) # joe is 1, plus 2 equals 3
+
+# order of operations and saving the variable
+
+funny = 21
+
+complex_joe = (33 + funny * 5) / ( 1 + 1 )
+
+print(complex_joe)
+
+print(complex_joe*2)
+
+print(complex_joe*4)
+
+complex_joe += 1
+
+print(complex_joe)
